1eb3d2faa3899c0abf4dd3e0d1459b7443a6523b
michele <michele.provolo@gmail.com>
2019-12-07 17:13:20 +0100

    ex ok

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Document</title>
+
+    <!-- Stampare una stringa verde se la
+variabile password passata in GET è
+uguale a “Boolean”, altrimenti stampare
+una stringa rossa. -->
+
+    <?php
+
+      $password = $_GET["password"];
+
+    ?>
+
+</head>
+<body>
+
+      <?php
+      if($password === "Boolean") {
+        echo "<h1 style='color:green'>stringa verde</h1>";
+        } else {
+          echo "<h1 style='color:red'>stringa rossa</h1>";
+        }
+
+?>
+</body>
+</html>
